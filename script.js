4c98f71ce372e46e6bba2ff62c754507515bf5e4
Fátima Vieira <devfvieira@gmail.com>
2026-03-29 15:57:42 -0300

    Tornei o modal dinâmico

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-const searchButton = document.getElementById('search-button');
+const searchButton = document.getElementById('search-button'); // opcional
+//nomeclatura: $searchButton ou searchButtonElement
 const overlay = document.getElementById('modal-overlay');
 const movieName = document.getElementById('movie-name');
 const movieYear = document.getElementById('movie-year');
-
 async function searchButtonClickHandler() {
   try {
-    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
+    let url = `http://www.omdbapi.com/?
+apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log('data: ', data);
+    if (data.Error) {
+      throw new Error('Filme não encontrado');
+    }
+    createModal(data);
     overlay.classList.add('open');
   } catch (error) {
-    console.error(error.message);
+    notie.alert({ type: 'error', text: error.message });
   }
 }
-
 function movieNameParameterGenerator() {
-  if (movieName.value === '')
-    throw new Error('O nome do filme deve ser informado'); //retorna um error
+  if (movieName.value === '') {
+    throw new Error('O nome do filme deve ser informado');
+  }
   return movieName.value.split(' ').join('+');
 }
-
 function movieYearParameterGenerator() {
   if (movieYear.value === '') {
     return '';
   }
   if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
-    throw new Error('Ano do filme inválido');
+    throw new Error('Ano do filme inválido.');
   }
   return `&y=${movieYear.value}`;
 }
-
 searchButton.addEventListener('click', searchButtonClickHandler);
